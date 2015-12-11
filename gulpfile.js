558c7fc09669c8cb38689722e22f57c779b489ec
shoukath <shoukhan@yahoo.com>
2015-12-11 18:49:14 +0000

    Adding NPM and GULP

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var imageop = require('gulp-image-optimization');
+ 
+gulp.task('images', function(cb) {
+    gulp.src(['assets/**/*.png']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('images')).on('end', cb).on('error', cb);
+});
+
+// Default Task
+gulp.task('default', ['images']);
